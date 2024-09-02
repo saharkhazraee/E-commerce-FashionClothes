56c380619efe28923e15b313af54a7a554668a02
saharkhazraee <159418657+saharkhazraee@users.noreply.github.com>
2024-09-02 17:08:51 +0330

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import AuthLayOut from './components/auth/layout'
+import Login from './pages/auth/login'
+import Register from './pages/auth/register'
+import AdminLayout from './components/adminView/layout'
+import AdminDashboard from './pages/adminView/dashboard'
+import AdminFeatures from './pages/adminView/features'
+import AdminProducts from './pages/adminView/products'
+import ShoppingView from './components/shoppingView/layout'
+import NotFound from './pages/notFound'
+import ShoppingHome from './pages/shoppingView/home'
+import ShoppingListing from './pages/shoppingView/listing'
+import ShoppingCheckOut from './pages/shoppingView/checkOut'
+import ShoppingAccount from './pages/shoppingView/account'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        <Route path='/auth' element={<AuthLayOut />}>
+          {/* child route's element */}
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+        </Route>
+        <Route path='/admin' element={<AdminLayout />}>
+          <Route path='dashboard' element={<AdminDashboard />} />
+          <Route path='features' element={<AdminFeatures />} />
+          <Route path='products' element={<AdminProducts />} />
+        </Route>
+        <Route path='/shop' element={<ShoppingView />}>
+          <Route path='home' element={<ShoppingHome />} />
+          <Route path='listing' element={<ShoppingListing />} />
+          <Route path='checkout' element={<ShoppingCheckOut />} />
+          <Route path='account' element={<ShoppingAccount />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ client/src/store/index.js
@@ -0,0 +1,10 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from "./auth-slice";
+
+const store=configureStore({
+  reducer:{
+    auth:authSliceReducer,
+  }
+})
+export default store
